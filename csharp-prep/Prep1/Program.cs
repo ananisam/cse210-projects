f93a08bf07175b3220126ee05949292dfde79462
ananisam <ananisk@gmail.com>
2024-03-06 17:45:30 +0000

    Renamed variables to conform with C# convention

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,17 +6,17 @@ class Program
     {
         // Asking the user for his or her first name and the storing it in a variable
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         // Asking the user for his or her Last Name and the storing it in a variable
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
         // Inserting a blank line
         Console.WriteLine();
         
         // Displaying the user input to the console
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
 
     }
 }
